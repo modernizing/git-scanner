a79610040889b29ecf74b8a019fe32a4a868c4f7
Phodal Huang <h@phodal.com>
2021-03-04 11:28:24 +0800

    feat: add scanner by years api

--- src/lib.rs
+++ src/lib.rs
@@ -9,9 +9,24 @@ extern crate derive_builder;
 #[macro_use]
 extern crate derive_getters;
 
+use crate::git::GitCalculator;
+use crate::git_logger::GitLogConfig;
+use crate::toxicity_indicator_calculator::ToxicityIndicatorCalculator;
+
 pub mod git_file_history;
 pub mod git_logger;
 pub mod git;
 pub mod git_file_future;
 pub mod git_user_dictionary;
 pub mod toxicity_indicator_calculator;
+
+pub fn scanner_by_years(git_years: u64) -> GitCalculator {
+    let calculator = GitCalculator::new(
+        GitLogConfig::default()
+            .include_merges(true)
+            .since_years(git_years),
+        true,
+    );
+
+    calculator
+}
